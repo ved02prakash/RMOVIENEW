1c8918aad3faac0c6dee491d9c20ab3ede024cd5
ved02prakash <ved02prakash.singh@gmail.com>
2023-08-01 16:34:14 +0530

    filter movies

--- src/genres/genere.model.ts
+++ src/genres/genere.model.ts
@@ -1,3 +1,8 @@
 export interface genereCreateDTO{
     name:string
 }
+
+export interface genreDTO{
+    id:number,
+    name:string
+}
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -1,13 +1,18 @@
 import { ReactElement } from "react";
 
 export default function Button(props: buttonProps){
-return <button disabled={props.disabled} className="btn btn-primary" type={props.type} onClick={props.onClick}>{props.children}</button>
+return <button disabled={props.disabled} 
+className={props.className} 
+type={props.type} 
+onClick={props.onClick}
+>{props.children}</button>
 }
 interface buttonProps{
     children?:React.ReactNode;
     onClick?():void;
-    type:"button" | "submit",
-    disabled:boolean
+    type:"button" | "submit";
+    disabled:boolean;
+    className?:string;
 }
 
 Button.defaultProps={
